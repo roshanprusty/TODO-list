bc1ce67390f10d98cff74ffcad6779a52143d8a6
Roshan Prusty <88710470+roshanprusty@users.noreply.github.com>
2023-04-29 15:20:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Form from "./components/Form";
 function App() {
   return (
     <div className="wrapper">
-      <h1>Todo List</h1>
+      <h1>Todo List - continuous integration and continuous deployment</h1>
+    
       <Form/>
     </div>
   );
